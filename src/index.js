2fbbaf0c643051a403835fa54789aef150c81b00
oladimejiadesina-123 <adesinaoladimeji04@gmail.com>
2022-06-24 15:00:03 +0100

    redux project

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from 'react-redux';
+import userReducer from "./features/user";
+import themeReducer from "./features/theme";
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+    theme: themeReducer,
+  },
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
